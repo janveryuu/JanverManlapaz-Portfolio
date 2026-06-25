38daa8bdd50e6f8172da6a42eb755e38c7790541
Janver Manlapaz <Janvermanlapaz@gmail.com>
2026-06-26 01:10:25 +0800

    feat: add resume pdf and link button

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -81,7 +81,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="/Janver_Manlapaz_Resume.pdf" target="_blank" rel="noopener noreferrer">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
