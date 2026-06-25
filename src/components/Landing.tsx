089855b5e795dc64f5751873c7487d60a479d55a
Janver Manlapaz <Janvermanlapaz@gmail.com>
2026-06-25 17:37:55 +0800

    Update mobile fallback image to janver-coolselfie

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -31,7 +31,7 @@ const Landing = ({ children }: PropsWithChildren) => {
           </div>
           {/* Mobile photo - shows only on mobile when 3D character is hidden */}
           <div className="mobile-photo">
-            <img src="/images/mypicnbg.png" alt="Janver Manlapaz" />
+            <img src="/images/janver-coolselfie.png" alt="Janver Manlapaz" />
           </div>
         </div>
         {children}
